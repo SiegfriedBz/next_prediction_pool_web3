944d206477e91f347256419659bebc1ab292eaa6
Siegfried <siegfried.bozza@yahoo.com>
2025-10-30 20:17:18 +0100

    Merge Develop into Main - update wording across app (#12)
    * feat(fe): Add Hero section and tabs layout for main page (#5)
    
    * feat(fe): Add Hero section and base layout structure
    
    * feat(fe): Add GameTabs component with tab sections structure
    
    * feat(fe): add create-game-form and related components (#7)
    
    * feat(fe): add mobile-wallet-support (#9)
    
    * update wording accross app (#11)

--- fe/app/_types/index.ts
+++ fe/app/_types/index.ts
@@ -1,41 +1,41 @@
-import { z } from "zod"
+import { z } from "zod";
 
-export type HexAddress = `0x${string}`
+export type HexAddress = `0x${string}`;
 
 export enum Network {
-  SEPOLIA = 11155111,
-  MUMBAI = 80001,
+	SEPOLIA = 11155111,
+	MUMBAI = 80001,
 }
-export const NetworkSchema = z.nativeEnum(Network)
-export type NetworkT = z.infer<typeof NetworkSchema>
+export const NetworkSchema = z.nativeEnum(Network);
+export type NetworkT = z.infer<typeof NetworkSchema>;
 
 export enum RoundStatus {
-  NotStarted,
-  Active,
-  Ended,
-  Resolved
+	NotStarted,
+	Active,
+	Ended,
+	Resolved,
 }
 
 export type Round = {
-  id: bigint
-  creator: HexAddress
-  priceFeed: HexAddress
-  target: bigint // target price
-  gteTotal: bigint
-  ltTotal: bigint
-  status: RoundStatus
-  end: bigint
-}
+	id: bigint;
+	creator: HexAddress;
+	priceFeed: HexAddress;
+	target: bigint; // target price
+	gteTotal: bigint;
+	ltTotal: bigint;
+	status: RoundStatus;
+	end: bigint;
+};
 
-export type RawBid = [bigint, bigint, boolean]
+export type RawBet = [bigint, bigint, boolean];
 
-export type Bid = {
-  amount: bigint // eth amount placed for this bid
-  time:  bigint// timestamp when user placed its bid on roundId
-  claimed: boolean
-}
+export type Bet = {
+	amount: bigint; // eth amount placed for this bet
+	time: bigint; // timestamp when user placed its bet on roundId
+	claimed: boolean;
+};
 
-export type RoundWithPlayerBid = Round & {
-  playerBid: Bid | null
-  playerIsWinner: boolean | null
-}
+export type RoundWithPlayerBet = Round & {
+	playerBet: Bet | null;
+	playerIsWinner: boolean | null;
+};
