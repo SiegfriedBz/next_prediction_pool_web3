01293dc81e8d5d136ac9c0dc23f2ac8e384af7f0
siegfriedbz <siegfried.bozza@yahoo.com>
2025-11-06 21:16:19 +0100

    feat(ui): remove motion + make Hero server component + add footer

--- fe/app/_components/typography/h6.tsx
+++ fe/app/_components/typography/h6.tsx
@@ -7,11 +7,19 @@ export const TypographyH6: FC<PropsWithChildren<Props>> = (props) => {
   const { className, children, ...rest } = props;
 
   return (
+<<<<<<< HEAD
     <h6
+=======
+    <h5
+>>>>>>> 9841a6a (feat(ui): remove motion + make Hero server component + add footer)
       {...rest}
       className={cn("text-sm font-light tracking-tight", className)}
     >
       {children}
+<<<<<<< HEAD
     </h6>
+=======
+    </h5>
+>>>>>>> 9841a6a (feat(ui): remove motion + make Hero server component + add footer)
   );
 };
