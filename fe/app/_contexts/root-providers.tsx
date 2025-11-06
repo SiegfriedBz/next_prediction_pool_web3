5ae3d9dd93852a95de81e3c21439d23b6d6a473f
Siegfried <siegfried.bozza@yahoo.com>
2025-11-06 19:51:34 +0100

    Merge Develop into Main - fix(wagmi): hydrate wagmi state from cookies in RootLayout (#36)
    * feat(fe): Add Hero section and tabs layout for main page (#5)
    
    * feat(fe): Add Hero section and base layout structure
    
    * feat(fe): Add GameTabs component with tab sections structure
    
    * feat(fe): add create-game-form and related components (#7)
    
    * feat(fe): add mobile-wallet-support (#9)
    
    * update wording accross app (#11)
    
    * feat(fe): Add “Bet on Games” Feature with Contract Integration (#13)
    
    * chore: update  dependencies
    
    * feat(fe): update create game form
    
    * feat(ui): add shared UI components for tables and dialogs
    
    * feat(fe): add hook to fetch all rounds data
    
    * feat(fe): add Bet on Games tab with betting form and tables
    
    * feat(fe): improve round data management and post-bet refresh (#15)
    
    * feat(fe): add RoundDataProvider and context
    
    Introduced a dedicated RoundDataProvider and RoundDataContext for managing all rounds state across the app.
    - Provides centralized access to rounds data, loading state, and refetch function.
    - Replaces legacy Providers context.
    
    * refactor(fe): improve useAllRoundsData hook
    
    * feat(fe): integrate round data provider and refetch
    
    * chore(fe): minor improvements and cleanup
    
    * Feature/fe improve UI (#17)
    
    * feat(header): make header responsive
    
    * feat(table-pagination): improve UI
    
    * refactor(create-game-form): improve UI and add comments
    
    * fix linters
    
    * feat(claim-rewards): add ResolvedRoundsWithPlayerDataProvider and refactor rounds context (#19)
    
    * refactor(context): rename and refactor RoundsProvider and useRounds hook
    
    * feat(context): add ResolvedRoundsWithPlayerDataProvider and related hooks
    
    * feat(claim-rewards): update ClaimRewards components using ResolvedRoundsWithPlayerDataProvider
    
    * chore: remove deprecated round data providers and hooks
    
    * refactor(rounds): rename and refactor RoundsProvider and useRounds hook
    
    * add stats-cards
    
    * Feature/fe metadata and analytics (#21)
    
    * chore(fe/analytics): integrate Vercel Analytics and Speed Insights
    
    * feat(hero): update Hero section
    
    * feat(ui): add Logo and integrate into Header
    
    * fe: update metadata with descriptive title and description
    
    * fe: add favicons, webmanifest, and remove unused public icons
    
    * feat(fe): Refactor Bet-on-Games & Claim-Rewards to use new Active/Resolved Rounds contexts (#23)
    
    * refactor(hooks): replace usePlayerBetsOnResolvedRounds with generic usePlayerBetsOnRounds
    
    * Introduce ActiveRoundsWithPlayerBetsProvider and related hooks
    
    * Add ResolvedRoundsWithPlayerBetsAndWinsProvider and related hooks
    
    * Remove old RoundsProvider and ResolvedRoundsWithPlayerDataProvider
    
    * Refactor use-resolved-rounds and use-rounds to use new providers
    
    * Improve BetOnGameForm and RowActions: proper React Query invalidation, tooltip & UI improvements
    
    * Update Bet-on-Games tab, table and stats card to use new contexts and hooks
    
    * Update Claim-Rewards tables, stats cards and tab to use new contexts and hooks
    
    * feat(fe): Polish UI and Extract Reusable Table Components (#25)
    
    * refactor: extract reusable table header and cell components
    
    * feat(ui): improve Bet-on-Games table columns and actions layout
    
    * feat(ui): polish Claim-Rewards table columns and row actions
    
    * chore(ui): refine button, tooltip, and side checkbox styling
    
    * feat(be): add PredictionPoolToken ERC1155 SC with Chainlink VRF integration (#27)
    
    * feat(tenderly): add  Web3 Action to mint ERC1155 on RoundResolved event (#29)
    
    * feat(fe): Add WebSocket Event Listener & Real-Time UI Updatesr (#31)
    
    * chore: update dependencies, next config, and ignore rules
    
    * feat(ui): update Hero section
    
    * feat(ui): update Toast
    
    * feat(chore): add prediction-pool-token config
    
    * feat: integrate Alchemy websocket and use-events hook
    
    * feat: add event listener switch and hooks, and NFT image display
    
    * add license (#33)
    
    * fix(wagmi): hydrate wagmi state from cookies in RootLayout (#35)

--- fe/app/_contexts/root-providers.tsx
+++ fe/app/_contexts/root-providers.tsx
@@ -3,15 +3,26 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { FC, PropsWithChildren } from "react";
-import { WagmiProvider } from "wagmi";
+import { type FC, type PropsWithChildren, useState } from "react";
+import {
+	type Config as WagmiConfig,
+	WagmiProvider,
+	type State as WagmiState,
+} from "wagmi";
 import { Toaster } from "@/components/ui/sonner";
 import { wagmiHttpConfig } from "../_config/wagmi";
 import { ThemeProvider } from "./theme-provider";
 
-const queryClient = new QueryClient();
+type Props = {
+	initialWagmiState: WagmiState | undefined;
+};
 
-export const RootProviders: FC<PropsWithChildren> = ({ children }) => {
+export const RootProviders: FC<PropsWithChildren<Props>> = (props) => {
+	const { initialWagmiState, children } = props;
+
+	const [wagmiConfig] = useState<WagmiConfig>(() => wagmiHttpConfig);
+	const [queryClient] = useState<QueryClient>(() => new QueryClient());
+
 	return (
 		<ThemeProvider
 			attribute="class"
@@ -19,7 +30,7 @@ export const RootProviders: FC<PropsWithChildren> = ({ children }) => {
 			enableSystem
 			disableTransitionOnChange
 		>
-			<WagmiProvider config={wagmiHttpConfig}>
+			<WagmiProvider config={wagmiConfig} initialState={initialWagmiState}>
 				<QueryClientProvider client={queryClient}>
 					<RainbowKitProvider>
 						{children}
