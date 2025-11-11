b27b0f568afb40e5cb547e8b1e447a7a72600736
siegfriedbz <siegfried.bozza@yahoo.com>
2025-11-11 12:10:31 +0100

    refactor(Constants_PredictionPool): update MIN_ROUND_DURATION

--- be/script/Constants_PredictionPool.sol
+++ be/script/Constants_PredictionPool.sol
@@ -6,5 +6,5 @@ contract Constants_PredictionPool {
     address public constant SEPOLIA_ETH_USD = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
     address public constant SEPOLIA_BTC_USD = 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43;
     address public constant SEPOLIA_DAI_USD = 0x14866185B1962B63C3Ea9E03Bc1da838bab34C19;
-    uint256 public constant MIN_ROUND_DURATION = 15 seconds;
+    uint256 public constant MIN_ROUND_DURATION = 2 minutes;
 }
